5e90dc2fc3ddb6976a641c7d82607726d5a9a46f
TaplierShiru <cool.danik01@yandex.ru>
2021-01-31 12:17:38 +0400

    fix(gans_controller): remove debug prints

--- makiflow/models/gans/trainers/gans_controller.py
+++ makiflow/models/gans/trainers/gans_controller.py
@@ -264,13 +264,6 @@ class GansController:
                         data_for_gen_x = data_for_gen_x[0]
 
                     gen_gen_data = wrapper_gen(data_for_gen_x, y_generator)
-                    print('i: ', self._generator_discriminator.get_model().get_inputs())
-                    print('o: ', self._generator_discriminator.get_model().get_outputs())
-                    print('la: ', self._generator_discriminator.get_labels())
-
-                    print('next1: ', next(gen_gen_data)[0][0].shape)
-                    print('next2: ', next(gen_gen_data)[0][0].shape)
-
                     info_gen = self._generator_discriminator.fit_generator(
                         generator=gen_gen_data,
                         epochs=epochs_generator,
